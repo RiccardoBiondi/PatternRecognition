3f898ba64f3402e23f97610662742122254274bf
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2019-12-21 17:00:48 +0100

    Implemented constructors , getter and setter

--- src/EDMatrix.hpp
+++ src/EDMatrix.hpp
@@ -1,35 +1,55 @@
 #ifndef EDMATRIX_HPP
 #define EDMATRIX_HPP
 
+#include <Eigen\Eigenvalues>
+/**
+ *  EDMatrix v2.0.0
+ *  Starting Generation: 2019-12-21
+ *  ----------------------------------------------------------
+ *  This fila contains the implementations of the template class EDMatrix and some external methods usefull to work with Euclidena Distance Matrix
+ *
+ * Requirments:
+ *   Eigen 3.7.0
+ *
+ */
 
 
-template<unsigned int N, unsigned int d>
-class EDMatrix : Eigen::MatrixXd {
+template<typename T, unsigned int N, unsigned int d>
+class EDMatrix{
+
+/**
+* EDMatrix class, provides methods to build and works with Euclidean Distance Matrix(EDM)
+* EDMatrix need Eigen 3.7.0 to work.
+* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+* @tparam T type of entries of the EDM, can be double, float or int
+* @tparam unsigned int N, number of points from which EDM is created. Is the dimension of the NxN EDM.
+* @tparam unsigned ind dimension of the euclidean space in which points live
+*/
 
   private:
-    Eigen::MatrixXd m_EDM;
-    EIgen::MatrixXd m_Mask;
+    Eigen::Matrix<T,N,N> m_EDM;
+    Eigen::Matrix<int,N,N> m_Mask;
 
   public:
     //Constructors
-    EDMatrix();
-    EDMatrix(t_EDM, t_Mask = Ones(N,N));
-    EDMatrix(const EDMatrix& o_Matrix);
+    EDMatrix(){};
+    EDMatrix(Eigen::Matrix<T,N,N> t_EDM,
+            Eigen::Matrix<int,N,N> t_Mask = Eigen::Matrix<int,N,N>::Ones()):
+              m_EDM(t_EDM),m_Mask(t_Mask){};
 
+    EDMatrix(const EDMatrix<T,N,d>& o_EDMatrix):m_EDM(o_EDMatrix.getEDM()),
+                                                m_Mask(o_EDMatrix.getMask()){};
     //operator
-
-    //getters, setter
-    MatrixXd getEDM() const;
-    MatrixXd getMask() const;
-
-    void setMask();
+    //EDMatrix<T,N,d> operator& = (const EDMatrix<T,N,d>& t_EDMatrix);
 
 
+    //getter and setter
+    Eigen::Matrix<T,N,N> getEDM() const;
+    Eigen::Matrix<int,N,N> getMask() const;
 
-    //Member functions
-
-    bool is_hollow();
-    bool is_symmetric();
+    void setEDM(const Eigen::Matrix<T,N,N>& t_EDM);
+    void setMask(const Eigen::Matrix<int,N,N>& t_Mask);
+    //methods
     bool is_positive();
     bool is_triang_inh();
     bool is_EDM();
@@ -38,10 +58,10 @@ class EDMatrix : Eigen::MatrixXd {
     void make_positive();
     void trim();
 
-    MatrixXd gc_matrix();
-    MatrixXd gramm();
+    Eigen::Matrix<T,N,N> gc_matrix();
+    Eigen::Matrix<T,N,N> gramm();
 
-    double frobenius_norm();
+    T frobenius_norm();
 
 
 
@@ -54,11 +74,68 @@ class EDMatrix : Eigen::MatrixXd {
 
 
 //Declaratin of externam methods
-template<unsigned int N, unsigned int d>
-EDMatrix<N,d> EVTreshold(EDMatrix<N,d> t_EDM);
+template<typename T,unsigned int N, unsigned int d>
+EDMatrix<T,N,d> EVTreshold(EDMatrix<T,N,d> t_EDM);
 
-template<unsigned int N, unsigned int d>
-MatrixXd ClassicalMDS(EDMAtrix<N,d> t_EDM);
+template<typename T, unsigned int N, unsigned int d>
+Eigen::Matrix<T,N,N> ClassicalMDS(EDMatrix<T,N,d> t_EDM);
+
+
+
+
+
+
+//Starting definitions
+
+//
+//Getter definition
+//
+template<typename T, unsigned int N, unsigned int d>
+Eigen::Matrix<T,N,N> EDMatrix<T,N,d>:: getEDM() const{
+  /**
+  * getter. @returns m_EDM.
+  *@see getMask
+  */
+  return m_EDM;
+}
+
+
+template<typename T, unsigned int N, unsigned int d>
+Eigen::Matrix<int,N,N> EDMatrix<T,N,d>:: getMask() const{
+  /**
+  * getter. @returns m_Mask
+  *@see getEDM
+  */
+  return m_Mask;
+}
+
+
+//
+//Setter Definitions
+//
+
+template<typename T, unsigned int N, unsigned int d>
+void EDMatrix<T,N,d>:: setEDM(const Eigen::Matrix<T,N,N>& t_EDM){
+  /**
+  * assign to m_EDM the t_EDM value
+  * @param const Eigen::Matrix<T,N,N>& t_EDM
+  */
+  m_EDM = t_EDM;
+}
+
+template<typename T,unsigned int N, unsigned int d>
+void EDMatrix<T,N,d>:: setMask(const Eigen::Matrix<int,N,N>& t_Mask){
+  /**
+  * set the value of m_Mask to t_Mask
+  * @param const Eigen:: Matrix<int,N,N>& t_Mask
+  */
+  m_Mask = t_Mask;
+}
+
+
+
+
+
 
 
 
