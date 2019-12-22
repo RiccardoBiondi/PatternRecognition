516af009a0e4448f790140eb4934ef1677b779f2
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2019-12-22 11:00:09 +0100

    Added test for boolen methods

--- test/EDMatrix_test.cpp
+++ test/EDMatrix_test.cpp
@@ -61,3 +61,58 @@ SCENARIO("getter, setter, costructors","[one]"){
 
 
 }
+
+
+
+
+
+
+
+
+
+SCENARIO("test the boolen methods", "[two]"){
+
+  GIVEN("An EDM and a not EDM matrix"){
+
+    Eigen:: MatrixXd A(4,4);
+      A << 0. , 8649., 6724.,17689.,
+        8649. , 0.   , 2704., 3600.,
+        6724. , 2704.,0.    ,12321.,
+        17689., 3600.,12321., 0.;
+
+        Eigen:: MatrixXd B(4,4);
+          B << 0. , 16., 17.,18.,
+              16. , 3. , 19., 20.,
+              17. , 49., -1 , 21.,
+              13. , 25., 21., 0.;
+
+    WHEN("Check the boolen metrods on the EDM"){
+      EDMatrix<double,4,4> D (A);
+      THEN("All methods returns true"){
+        REQUIRE(D.is_hollow());
+        REQUIRE(D.is_positive());
+        REQUIRE(D.is_symmetric());
+        REQUIRE(D.is_triang_inh());
+        REQUIRE(D.is_EDM());
+      }
+
+
+    }
+
+    WHEN("Check boolen methods on the not EDM"){
+      EDMatrix<double,4,4> D (B);
+      THEN("All methods return false"){
+
+        REQUIRE_FALSE(D.is_hollow());
+        REQUIRE_FALSE(D.is_positive());
+        REQUIRE_FALSE(D.is_symmetric());
+        REQUIRE_FALSE(D.is_triang_inh());
+        REQUIRE_FALSE(D.is_EDM());
+
+      }
+  }
+
+
+
+}
+}
